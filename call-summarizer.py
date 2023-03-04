c09f17cf71aa568d981b0ce15ec1fc064e9da9f4
nicholastwai <57726227+nicholastwai@users.noreply.github.com>
2023-03-04 17:27:24 -0500

    added mp3 transcription functionality

--- call-summarizer.py
+++ call-summarizer.py
@@ -5,54 +5,31 @@
 # Import the required libraries
 import openai
 import streamlit as st
+import whisper
+from tempfile import NamedTemporaryFile
 
 # Set the GPT-3 API key
 openai.api_key = st.secrets["pass"]
 
 audio_file = st.file_uploader("Upload an audio file", type=["mp3"])
-
 if audio_file:
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-
-transcript = st.text_area("Enter your phone call transcript to summarize")
+    with NamedTemporaryFile(suffix="mp3") as temp:
+        temp.write(audio_file.getvalue())
+        temp.seek(0)
+        model = whisper.load_model("base")
+        result = model.transcribe(temp.name)
+        transcript = result["text"]
+else:
+    transcript = st.text_area("Enter your phone call transcript to summarize")
 
 if st.button("Generate Summary",type='primary'):
     # Use GPT-3 to generate a summary of the article
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt="Please summarize this phone call transcript for me in a few sentences: " + transcript,
+            prompt="Please summarize this phone call transcript between an insuree and their insurer in a few sentences: " + transcript,
             max_tokens = 516,
             temperature = 0.5,
         )
         # Print the generated summary
         res = response["choices"][0]["text"]
         st.success(res)
-
-'''
-if output_size == "To-The-Point":
-    out_token = 50
-elif output_size == "Concise":
-    out_token = 128
-else:
-    out_token = 516
-
-if len(article_text)>100:
-    if st.button("Generate Summary",type='primary'):
-    # Use GPT-3 to generate a summary of the article
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt="Please summarize this scientific article for me in a few sentences: " + article_text,
-            max_tokens = out_token,
-            temperature = 0.5,
-        )
-        # Print the generated summary
-        res = response["choices"][0]["text"]
-        st.success(res)
-        st.download_button('Download result', res)
-else:
-    st.warning("Not enough words to summarize!")
-
-output_size = st.radio(label = "What kind of output do you want?", 
-                    options= ["To-The-Point", "Concise", "Detailed"])
-
-'''
